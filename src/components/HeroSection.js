dfcd28dc3fe04db7198740a1b864475ebc7253bb
annpermi <perminova.anna85@gmail.com>
2021-07-06 17:37:40 -0400

    Cards component

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -7,13 +7,12 @@ function HeroSection() {
     return (
         <div className='hero-container'>
             <video src="/videos/video-1.mp4" autoPlay loop muted poster="../images/img-home.jpg"/>
-                {/* <source src="./videos/video-1.mp4" type='video/mp4'/>
-            </video> */}
             <h1>ADVENTURE AWAITS</h1>
             <p>What are you waiting for?</p>
             <div className="hero-btns">
                 <Button className='btns' buttonStyle='btn--outline' buttonSize='btn--large'>Get Started</Button>
-                <Button className='btns' buttonStyle='btn--primary' buttonSize='btn--large'>Watch Trailer</Button>
+                <Button className='btns' buttonStyle='btn--primary' buttonSize='btn--large'>Watch Trailer
+                <i className='fa fa-play-circle'></i></Button>
             </div>
         </div>
     )
